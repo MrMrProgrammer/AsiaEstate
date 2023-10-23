af368dce507f44b49a97a90eb695afe5ea5e10c2
MrMrProgrammer <mr.mr.programmer@gmail.com>
2023-10-23 22:56:55 +0330

    nothing

--- BaseConfig/views.py
+++ BaseConfig/views.py
@@ -4,4 +4,9 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, 'BaseConfig/home.html')
+
+    context = {
+
+    }
+
+    return render(request, 'BaseConfig/home.html', context)
