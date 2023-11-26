e114a80bd794351cb9ba6c6b0930fc2282f47d4a
MrMrProgrammer <mr.mr.programmer@gmail.com>
2023-11-26 23:27:32 +0330

    add per page ad

--- Ads/urls.py
+++ Ads/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('Land', views.show_land_ads, name='showLandAds'),
     path('Villa', views.show_villa_ads, name='showVillaAds'),
     path('Apartment', views.show_apartment_ads, name='showApartmentAds'),
+    path('signle-ad/<str:db_name>/<int:ad_id>', views.show_single_ad, name='signle-ad'),
 ]
