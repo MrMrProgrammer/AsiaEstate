27f1cd8a5e0f2bc86f40ba0e38fad2cc279a37a2
MrMrProgrammer <mr.mr.programmer@gmail.com>
2023-12-01 00:43:26 +0330

    nothing

--- AsiaEstate/settings.py
+++ AsiaEstate/settings.py
@@ -100,8 +100,8 @@ WSGI_APPLICATION = 'AsiaEstate.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-        'NAME': '/usr/src/app/db/db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'NAME': '/usr/src/app/db/db.sqlite3',
 
     }
 }
