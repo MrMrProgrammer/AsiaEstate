73b698b32e1d30ea053b19e596c43223deecbeea
MrMrProgrammer <mr.mr.programmer@gmail.com>
2023-12-03 09:00:58 +0330

    add  verbose name in models

--- AboutUs/models.py
+++ AboutUs/models.py
@@ -1,21 +1,18 @@
 from django.db import models
 
-
-# Create your models here.
-
 class AboutUs(models.Model):
-    image = models.ImageField(upload_to='AboutUs', null=True, blank=True)
+    image = models.ImageField(upload_to='AboutUs', null=True, blank=True, verbose_name='تصویر')
 
-    header_1 = models.CharField(max_length=100, null=True, blank=True)
-    body_1 = models.TextField(null=True, blank=True)
+    header_1 = models.CharField(max_length=100, null=True, blank=True, verbose_name='عنوان ۱')
+    body_1 = models.TextField(null=True, blank=True, verbose_name='پاراگراف ۱')
 
-    header_2 = models.CharField(max_length=100, null=True, blank=True)
-    body_2 = models.TextField(null=True, blank=True)
+    header_2 = models.CharField(max_length=100, null=True, blank=True, verbose_name='عنوان ۲')
+    body_2 = models.TextField(null=True, blank=True, verbose_name='پاراگراف ۲')
 
-    header_3 = models.CharField(max_length=100, null=True, blank=True)
-    body_3 = models.TextField(null=True, blank=True)
+    header_3 = models.CharField(max_length=100, null=True, blank=True, verbose_name='عنوان ۳')
+    body_3 = models.TextField(null=True, blank=True, verbose_name='پاراگراف ۳')
 
-    is_active = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True, verbose_name='فعال / غیرفعال')
 
     class Meta:
         db_table = 'AboutUs'
