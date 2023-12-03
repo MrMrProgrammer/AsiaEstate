c302b9cae9048faa34278dd7f325837c503b2741
MrMrProgrammer <mr.mr.programmer@gmail.com>
2023-12-03 09:01:09 +0330

    add  verbose name in models

--- ContacUs/models.py
+++ ContacUs/models.py
@@ -4,13 +4,13 @@ import pytz
 
 
 class ContactUs(models.Model):
-    name = models.CharField(max_length=100)
-    phone_number = models.CharField(max_length=20)
-    message = models.TextField()
-    datetime = models.DateTimeField(auto_now_add=True, verbose_name='زمان ارسال پیام', null=True, blank=True)
+    name = models.CharField(max_length=100, verbose_name='')
+    phone_number = models.CharField(max_length=20, verbose_name='')
+    message = models.TextField(verbose_name='')
+    datetime = models.DateTimeField(auto_now_add=True, null=True, blank=True, verbose_name='زمان ارسال پیام')
 
     def jalali_created_at(self):
-        tz = pytz.timezone('Asia/Tehran')  # منطقه زمانی تهران
+        tz = pytz.timezone('Asia/Tehran')
         dt = self.created_at.astimezone(tz)
         j_date = jdatetime.datetime.fromgregorian(datetime=dt)
         return j_date.strftime('%d-%m-%Y  |  %H:%M')
