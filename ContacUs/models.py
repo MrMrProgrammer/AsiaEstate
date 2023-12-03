ebc74c3c1f50f894ae0597d295b899cb42199867
MrMrProgrammer <mr.mr.programmer@gmail.com>
2023-12-03 09:07:56 +0330

    edit some verbosname in models and admin

--- ContacUs/models.py
+++ ContacUs/models.py
@@ -4,9 +4,9 @@ import pytz
 
 
 class ContactUs(models.Model):
-    name = models.CharField(max_length=100, verbose_name='')
-    phone_number = models.CharField(max_length=20, verbose_name='')
-    message = models.TextField(verbose_name='')
+    name = models.CharField(max_length=100, verbose_name='نام و نام خانوادگی')
+    phone_number = models.CharField(max_length=20, verbose_name='شماره تماس')
+    message = models.TextField(verbose_name='پیام')
     datetime = models.DateTimeField(auto_now_add=True, null=True, blank=True, verbose_name='زمان ارسال پیام')
 
     def jalali_created_at(self):
